e648633233c8d05a47e9e860889bd8e3e85ebad4
Vincent Sijben <36117924+vincentsijben@users.noreply.github.com>
2022-11-17 09:36:11 +0100

    Add files via upload

---
+++ eck/template/water/calculator.php
@@ -0,0 +1,28 @@
+<div class="calculator_container">
+    <div>
+        <h3 class="medium_title">Hoeveel water heb je aan vandaag?</h3>
+
+        <div id="checkboxHolder">
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">Jeans</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">Nog iets</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">iets anders</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">bedenk et maar</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">nog een</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">nog ene</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">weer eentje</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">nog een</label></input><br></div>
+            <div class="checkbox"><input type="checkbox" value="1"><label for="checkbox">laatse</label></input><br></div>
+        </div>
+
+
+        <button id="waterButton">Reken uit</button>
+
+        <div id="waterText">
+            <h4><span id="waterAmount">...</span> liter water</h4>
+            <h5>Dat is <span id="yearAmount"></span> jaar aan drinkwater voor een persoon.</h5>
+        </div>
+
+
+
+    </div>
+</div>
