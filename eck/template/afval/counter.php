933816d3feb1978af8fd7bf75be98bbc216ee950
Vincent Sijben <36117924+vincentsijben@users.noreply.github.com>
2022-11-17 09:45:12 +0100

    Delete eck directory

--- eck/template/afval/counter.php
+++
@@ -1,8 +0,0 @@
-<section class="full_width counter">
-    <div class="double_flex padding counter">
-            <span id="seconds"></span>
-        <div class="counterDescription">
-            <p>Vrachtwagens met kleding zijn weggegooid sinds je op deze pagina bent.</p>
-        </div>
-    </div>
-</section>
