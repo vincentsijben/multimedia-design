e83cbd21e16429101029d6a5cceb197b6eeff13a
Vincent Sijben <36117924+vincentsijben@users.noreply.github.com>
2022-11-17 09:44:24 +0100

    Add files via upload

---
+++ fast-fashion-forensics/template/general/nav.php
@@ -0,0 +1,31 @@
+<div class="top_bar" id="top_bar">
+    <div class="backtohome">
+        <a href="index.html"><img src="img/arrow-left.svg" alt="arrow to left">Home</a>
+    </div>
+    <div class="navigation">
+        <div class="navigation_item">
+            <a href="afval.html"><img src="img/afval.png" name="afval"></a>
+            <h5 class="navigation_title">Afval</h5>
+        </div>
+        <div  class="navigation_item">
+            <a href="co2.html"><img src="img/co2.png"></a>
+            <h5 class="navigation_title">CO2</h5>
+        </div>
+        <div  class="navigation_item">
+            <a href="kinderarbeid.html"><img src="img/naaimachine.png"></a>
+            <h5 class="navigation_title">kinderarbeid</h5>
+        </div>
+        <div  class="navigation_item">
+            <a href="plastic.html"><img src="img/blouse.png"></a>
+            <h5 class="navigation_title">Plastic</h5>
+        </div>
+        <div  class="navigation_item">
+            <a href="ontbossing.html"><img src="img/ontbossing.png"></a>
+            <h5 class="navigation_title">Ontbossing</h5>
+        </div>
+        <div  class="navigation_item">
+            <a href="water.html"><img src="img/jeans.png"></a>
+            <h5 class="navigation_title">Water</h5>
+        </div>
+    </div>
+</div>
